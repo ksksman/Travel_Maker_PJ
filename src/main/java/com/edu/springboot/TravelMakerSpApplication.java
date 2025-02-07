31ae4629268f46b48ce4d3c1f2820237af2698eb
ksksman <Ks062567@gmail.com>
2025-02-07 18:39:08 +0900

    첫번째커밋

---
+++ src/main/java/com/edu/springboot/TravelMakerSpApplication.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMakerSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMakerSpApplication.class, args);
+	}
+
+}
