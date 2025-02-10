4e6c6a3f409add243703aa2f72606300ea8d7c13
ksksman <Ks062567@gmail.com>
2025-02-10 15:55:04 +0900

    합체

--- Travel_Maker_Re/KosmoProject/src/App.jsx
+++ Travel_Maker_Re/KosmoProject/src/App.jsx
@@ -6,14 +6,16 @@ import './App.css';
 import Header from './components/Header';
 import SideMenu from './components/SideMenu';
 
-// 첫 번째 프로젝트 컴포넌트
+// 여행 관련 컴포넌트
 import HomePage from './pages/HomePage';
 import MyPage from './pages/MyPage';
 import EditProfile from './pages/EditProfile';
 import TravelList from './components/TravelList';
 import TravelDetail from './components/TravelDetail';
+import CreateTripPage from './Pages/CreateTripPage';
+import PlanTripPage from "./pages/PlanTripPage";
 
-// 두 번째 프로젝트 컴포넌트
+// 로그인 & 회원가입 관련 컴포넌트
 import LoginPage from './Pages/LoginPage';
 import IDLoginPage from './pages/IDLoginPage';
 import SignupAgreement from './pages/SignupAgreement';
@@ -31,19 +33,20 @@ const App = () => {
 
     return (
         <Router>
-            {/* 공통 Header와 SideMenu */}
             <Header toggleMenu={toggleMenu} />
             <SideMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
             <Routes>
-                {/* 첫 번째 프로젝트 라우트 */}
+                {/* 여행 관련 라우트 */}
                 <Route path="/" element={<HomePage />} />
                 <Route path="/mypage" element={<MyPage />} />
                 <Route path="/edit-profile" element={<EditProfile />} />
                 <Route path="/trips" element={<TravelList />} />
                 <Route path="/trips/:id" element={<TravelDetail />} />
+                <Route path="/create-trip" element={<CreateTripPage />} />
+                <Route path="/plan-trip" element={<PlanTripPage />} />
 
-                {/* 두 번째 프로젝트 라우트 */}
+                {/* 로그인 및 회원가입 라우트 */}
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/id-login" element={<IDLoginPage />} />
                 <Route path="/signup-agreement" element={<SignupAgreement />} />
